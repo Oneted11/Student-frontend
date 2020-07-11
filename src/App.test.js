a63b44d8765f462d88436a3e66347392e790decf
Oneted11 <muriithited@gmail.com>
2020-07-11 12:27:00 +0300

    first test literary ever

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Home link ie React mern stack app', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/React MERN Stack App/i);
   expect(linkElement).toBeInTheDocument();
 });
