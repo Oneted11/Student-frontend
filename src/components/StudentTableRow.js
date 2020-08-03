99ab44e77b0b7aa63849ddd7f8248a59e864ddff
Oneted11 <muriithited@gmail.com>
2020-08-03 12:28:24 +0300

    changed from local to heroku backend

--- src/components/StudentTableRow.js
+++ src/components/StudentTableRow.js
@@ -12,7 +12,7 @@ export default class StudentTableRow extends Component {
   deleteStudent() {
     axios
       .delete(
-        "http://localhost:4000/students/delete-student/" + this.props.obj._id
+        "https://git.heroku.com/student-end-back.git/students/delete-student/" + this.props.obj._id
       )
       .then((res) => {
         console.log("Student successfully deleted!");
--- src/components/student-list.component.js
+++ src/components/student-list.component.js
@@ -14,7 +14,7 @@ export default class StudentList extends Component {
   }
 
   componentDidMount() {
-    axios.get('http://localhost:4000/students/')
+    axios.get('https://git.heroku.com/student-end-back.git/students/')
       .then(res => {
         this.setState({
           students: res.data
